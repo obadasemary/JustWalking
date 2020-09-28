8981bc9d7c6f06c14045e0837dda09da44dbda7e
Abdelrahman Mohamed <obada.semary@gmail.com>
2020-09-28 17:23:24 +0300

    Initial Commit

---
+++ JustWalking/JustWalkingApp.swift
@@ -0,0 +1,17 @@
+//
+//  JustWalkingApp.swift
+//  JustWalking
+//
+//  Created by Abdelrahman Mohamed on 28.09.2020.
+//
+
+import SwiftUI
+
+@main
+struct JustWalkingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
